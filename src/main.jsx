0751b9a54f032b5073a96c1cc739e63ee7f56c8a
UmaBadenehal <104186314+UmaBadenehal@users.noreply.github.com>
2022-09-13 19:33:00 +0530

    1 file modified

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,18 @@ const rootElement = document.getElementById('root');
 render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} >
+      <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />} />
-       </Route> 
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
     </Routes>
   </BrowserRouter>,
   rootElement
